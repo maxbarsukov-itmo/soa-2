1229a352347ea56eb83c9d25117d56f2a2ced6c4
maxbarsukov <maximbarsukov@bk.ru>
2025-11-14 16:25:42 +0300

    feat(api-tests): :sparkles: use api gateway

--- api-tests/conftest.py
+++ api-tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
-PEOPLE_URL = "https://localhost:51313/api/v1"
-DEMOGRAPHY_URL = "https://localhost:51312/api/v1"
+BASE_GATEWAY_URL = "http://localhost:8765/api/v1"
+PEOPLE_URL = BASE_GATEWAY_URL
+DEMOGRAPHY_URL = BASE_GATEWAY_URL
 
 def pytest_configure(config):
     pytest.PEOPLE_URL = PEOPLE_URL
